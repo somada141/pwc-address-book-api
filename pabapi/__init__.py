a19a925e0b3fc6640afed5a821706f833d9dfc8d
Adamos Kyriakou <somada141@gmail.com>
2017-08-26 17:22:19 +1000

    Added a new `config.py` boilerplate module responsible for ingesting and validating JSON configuration files with settings necessary for the application to operate.

--- pabapi/__init__.py
+++ pabapi/__init__.py
@@ -2,6 +2,8 @@
 
 """Top-level package for pwc-address-book-api."""
 
+import config
+
 __author__ = """Adamos Kyriakou"""
 __email__ = 'somada141@gmail.com'
 __version__ = '0.1.0'
---
+++ pabapi/config.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+
+import ujson
+import validictory
+import attrdict
+
+import excs
+
+config_schema_default = {
+    "type": "object",
+    "required": [
+        # General Settings.
+        "log_level",
+        # SQL Server Configuration Settings.
+        "sql_host", "sql_port", "sql_username", "sql_password", "sql_db"
+    ],
+    "properties": {
+        "log_level": {
+            "type": "string",
+            "description": ("The minimum level of `logging` messages that will "
+                            "be emitted"),
+            "enum": [
+                "DEBUG",
+                "INFO",
+                "WARNING",
+                "ERROR",
+                "CRITICAL"
+            ]
+        },
+        # SQL Server Configuration Settings.
+        "sql_host": {
+            "type": "string", "description": "MySQL server host."
+        },
+        "sql_port": {
+            "type": "integer", "description": "MySQL server port."
+        },
+        "sql_username": {
+            "type": "string", "description": "MySQL server username."
+        },
+        "sql_password": {
+            "type": "string", "description": "MySQL server password."
+        },
+        "sql_db": {
+            "type": "string", "description": "MySQL server database name."
+        },
+    }
+}
+
+
+def load_config_file(fname_config_file):
+    if (
+            not os.path.exists(fname_config_file) or
+            not os.path.isfile(fname_config_file)
+    ):
+        msg = "Config file '{0}' not found or not a file."
+        msg_fmt = msg.format(fname_config_file)
+        raise excs.ConfigFileNotFound(msg_fmt)
+
+    with open(fname_config_file, str("r")) as finp:
+        config = ujson.load(finp)
+
+    return config
+
+
+def validate_config(config_instance, config_schema=None):
+    if config_schema is None:
+        config_schema = config_schema_default
+
+    validictory.validate(
+        data=config_instance,
+        schema=config_schema,
+        required_by_default=False,
+        blank_by_default=True
+    )
+
+    return True
+
+
+def import_config(fname_config_file):
+    config = load_config_file(fname_config_file=fname_config_file)
+
+    validate_config(config_instance=config, config_schema=config_schema_default)
+
+    return attrdict.AttrDict(config)
