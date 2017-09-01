be36b790cbb144d18ad2883e6baef919a1b2983e
Adamos Kyriakou <somada141@gmail.com>
2017-09-02 08:18:44 +1000

    Updated the Python dependencies.

--- setup.py
+++ setup.py
@@ -12,21 +12,39 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'wheel==0.29.0',
+    'urllib3==1.15.1',
+    'pyasn1==0.1.9',
+    'pyOpenSSL==16.2.0',
+    'ndg-httpsclient==0.4.0',
+    'colorlog==2.10.0',
+    'ujson==1.35',
+    'validictory==1.1.0',
+    'attrdict==2.0.0',
+    'decorator==4.1.2',
+    'MySQL-python==1.2.5',
+    'SQLAlchemy==1.1.13',
+    'chardet==3.0.4',
+    'falcon==1.2.0',
+    'tox==2.3.1',
+    'coverage==4.1',
+    'Sphinx==1.4.8',
+    'unicodecsv==0.14.1',
+    'falcon-multipart==0.2.0',
 ]
 
-setup_requirements = [
-    # TODO(somada141): put setup requirements (distutils extensions, etc.) here
-]
+setup_requirements = []
 
 test_requirements = [
-    # TODO: put package test requirements here
+    'coverage',
+    'nose',
 ]
 
 setup(
     name='pabapi',
     version='0.1.0',
-    description="API backend for a simple address book implemented for the PwC interview process.",
+    description=("API backend for a simple address book implemented for the "
+                 "PwC interview process."),
     long_description=readme + '\n\n' + history,
     author="Adamos Kyriakou",
     author_email='somada141@gmail.com',
